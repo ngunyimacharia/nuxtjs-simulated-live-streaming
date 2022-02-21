ea3e2f5b1a1a735c581eb0d3d93a8322d3779a6e
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-02-21 14:18:17 +0200

    Simulate live streaming

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,58 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1 class="m-5 text-gray-800 text-lg font-bold text-center">
+      Simulated live streaming
+    </h1>
+    <video
+      id="video-player"
+      controls
+      muted
+      class="cld-video-player cld-fluid vjs-live cld-video-player-skin-dark w-2/3 h-96 mx-auto" 
+    />
+    <p class="m-5 text-gray-800 text-sm text-center">
+      Video by Taryn Elliott from Pexels
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  data(){
+    return {
+      cld:null,
+      video:'nuxtjs-simulated-live-streaming/video'
+    };
+  },
+
+  mounted(){
+    this.cld = cloudinary.Cloudinary.new({ cloud_name: 'hackit-africa' });
+    let demoplayer = this.cld.videoPlayer('video-player').width(600);
+    demoplayer.source(this.video);
+
+    var liveControls = document.getElementsByClassName("vjs-live-control");
+
+    demoplayer.on("play", function () {
+      liveControls[0].classList.remove("vjs-hidden");
+      
+      demoplayer.currentTime(
+        parseInt((new Date).getSeconds() % demoplayer.duration())
+      ); 
+    });
+  },
+  
 }
 </script>
+
+<style>
+
+body {
+  font-family: sans-serif;
+}
+.cld-video-player .vjs-control-bar .vjs-progress-control {
+  display: none;
+}
+div.vjs-control-bar > div.vjs-live-control.vjs-control {
+  display: block;
+}
+
+</style>
